f8de3ee48bc0664a254678dec372cf45066e45e3
Andrea Superchi <andrea.superchi@mail.polimi.it>
2020-05-15 17:24:47 +0200

    Aggiunta dei Bean di Campaign e Image

---
+++ Progetto_TIW/src/it/polimi/tiw/project/beans/Image.java
@@ -0,0 +1,96 @@
+package it.polimi.tiw.project.beans;
+
+import java.sql.*;
+
+public class Image {
+	private int id;
+	private Blob photo;
+	private double latitude;
+	private double longitude;
+	private String county;
+	private String municipality;
+	private String source;
+	private Date date;
+	private String resolution;
+	private int idCampaign;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public Blob getPhoto() {
+		return photo;
+	}
+	
+	public void setPhoto(Blob photo) {
+		this.photo = photo;
+	}
+	
+	public double getLatitude() {
+		return latitude;
+	}
+	
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	
+	public double getLongitude() {
+		return longitude;
+	}
+	
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	
+	public String getCounty() {
+		return county;
+	}
+	
+	public void setCounty(String county) {
+		this.county = county;
+	}
+	
+	public String getMunicipality() {
+		return municipality;
+	}
+	
+	public void setMunicipality(String municipality) {
+		this.municipality = municipality;
+	}
+	
+	public String getSource() {
+		return source;
+	}
+	
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String getResolution() {
+		return resolution;
+	}
+	
+	public void setResolution(String resolution) {
+		this.resolution = resolution;
+	}
+	
+	public int getIdCampaign() {
+		return idCampaign;
+	}
+	
+	public void setIdCampaign(int idCampaign) {
+		this.idCampaign = idCampaign;
+	}
+}
