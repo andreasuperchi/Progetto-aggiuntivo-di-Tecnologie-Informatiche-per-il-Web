ca8bac0bd00ab84cdedecec90ccff970162eb29d
Andrea Superchi <andrea.superchi@mail.polimi.it>
2020-05-26 17:42:57 +0200

    Aggiunta di alcuni filtri.

---
+++ Progetto_TIW/src/it/polimi/tiw/filters/ManagerChecker.java
@@ -0,0 +1,51 @@
+package it.polimi.tiw.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import it.polimi.tiw.project.beans.User;
+
+@WebFilter("/ManagerChecker")
+public class ManagerChecker implements Filter {
+
+    public ManagerChecker() {
+    }
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println("Manager checker filter executing...");
+		
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		
+		String loginPath = httpRequest.getServletContext().getContextPath() + "/index.html";
+		
+		HttpSession session = httpRequest.getSession();
+		
+		User user = null;
+		user = (User) session.getAttribute("user");
+		
+		if (!user.getRole().equals("manager")) {
+			httpResponse.sendRedirect(loginPath);
+			return;
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
